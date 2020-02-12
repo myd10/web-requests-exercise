6861e8fbe94947b09a558c832d49e1b77ce03fed
myd10 <myd10@georgetown.edu>
2020-02-12 17:42:36 -0500

    Import requests

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # web-requests-exercise
+
+create repo named web-requests-exercise
+clone
+open in github desktop and then in vsc
+navigate there from commmand line
+
+create virtual env
+conda create -n requests-env python=3.7
--- get_data.py
+++ get_data.py
@@ -1,1 +1,22 @@
-print("request some stuff")
+# get_data.py
+
+print("REQUESTING SOME DATA FROM THE INTERNET...")
+
+import requests
+
+request_url = "https://raw.githubusercontent.com/prof-rossetti/intro-to-python/master/data/products/1.json"
+print("URL: ", request_url)
+
+
+response = requests.get(request_url)
+print(type(response))
+#print(dir(response)),  shows what you can do with the "response"
+
+
+
+print(response.status_code)
+print(response.text)
+
+breakpoint()
+
+
